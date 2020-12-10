edf3327f4d5f8a4f1646db2f44c369eee0be0c39
SrMoffat <ngigemoffat@gmail.com>
2020-12-10 08:11:49 +0300

    add types and mock resolvers

---
+++ src/resolvers/queries/index.js
@@ -0,0 +1,57 @@
+// Fetch Categories
+const fetchCategories = () => {
+    return {
+        categories: [
+            "animal",
+            "career",
+            "celebrity",
+            "dev",
+            "explicit",
+            "fashion",
+            "food",
+            "history",
+            "money",
+            "movie",
+            "music",
+            "political",
+            "religion",
+            "science",
+            "sport",
+            "travel"
+        ]
+    };
+};
+
+// Fetch Random Joke
+const fetchRandomJoke = () => {
+    return {
+        "categories": [],
+        "created_at": "2020-01-05 13:42:25.099703",
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+        "id": "j8eVPeCBTpiuKxlsWRRagg",
+        "updated_at": "2020-01-05 13:42:25.099703",
+        "url": "https://api.chucknorris.io/jokes/j8eVPeCBTpiuKxlsWRRagg",
+        "value": "Chuck Norris throws massive Roman orgies for Lent."
+    };
+};
+
+// Fetch Category Joke
+const fetchCategoryJoke = () => {
+    return {
+        "categories": [
+            "animal"
+        ],
+        "created_at": "2020-01-05 13:42:19.104863",
+        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+        "id": "zjuwql5ns-mklqumqezlhg",
+        "updated_at": "2020-01-05 13:42:19.104863",
+        "url": "https://api.chucknorris.io/jokes/zjuwql5ns-mklqumqezlhg",
+        "value": "Chuck Norris can skeletize a cow in two minutes."
+    }
+};
+
+export default {
+    fetchCategories,
+    fetchCategoryJoke,
+    fetchRandomJoke
+};
