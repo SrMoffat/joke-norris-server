08cc6b15f2d76b166d33b3ffd7e94e12b0e9d85e
4fr0c0d3 <31407254+SrMoffat@users.noreply.github.com>
2020-12-11 01:38:14 +0300

    add logic for category joke (#6)

--- src/resolvers/queries/index.js
+++ src/resolvers/queries/index.js
@@ -1,11 +1,12 @@
 const fetch = require('node-fetch');
 
+const BASE_URL = "https://api.chucknorris.io"; // TODO: add to .env
 /**
  * Fetch categories 
  * @returns
  */
 const fetchCategories = async () => {
-    const response =  await fetch("https://api.chucknorris.io/jokes/categories");
+    const response =  await fetch(`${BASE_URL}/jokes/categories`);
     const categories = await response.json();
     return {
         categories
@@ -17,24 +18,21 @@ const fetchCategories = async () => {
  * @returns
  */
 const fetchRandomJoke = async () => {
-    const response =  await fetch("https://api.chucknorris.io/jokes/random");
+    const response =  await fetch(`${BASE_URL}/jokes/random`);
     const randomJoke = await response.json();
     return randomJoke;
 };
 
-// Fetch Category Joke
-const fetchCategoryJoke = () => {
-    return {
-        "categories": [
-            "animal"
-        ],
-        "created_at": "2020-01-05 13:42:19.104863",
-        "icon_url": "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
-        "id": "zjuwql5ns-mklqumqezlhg",
-        "updated_at": "2020-01-05 13:42:19.104863",
-        "url": "https://api.chucknorris.io/jokes/zjuwql5ns-mklqumqezlhg",
-        "value": "Chuck Norris can skeletize a cow in two minutes."
-    }
+/**
+ * Fetch a random joke in a selected category
+ * @param {{cateory: string;}} joke
+ * @returns
+ */
+const fetchCategoryJoke = async (parent, args, context, info) => {
+    const { category } = args;
+    const response = await fetch(`${BASE_URL}/jokes/random?category=${category}`);
+    const categoryJoke = await response.json();
+    return categoryJoke;
 };
 
 module.exports = {
